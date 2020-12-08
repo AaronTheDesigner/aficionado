44a3b2069fdf36d182aa7067876517091ceea794
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-08 15:07:47 -0500

    usercontroller => authcontroller userservice added

---
+++ src/main/java/com/aficionado/controllers/AuthController.java
@@ -0,0 +1,47 @@
+package com.aficionado.controllers;
+
+import com.aficionado.models.User;
+import com.aficionado.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    //get routes
+    @GetMapping(value="/signup")
+    public String signUp() {
+        return "signup";
+    }
+
+    @GetMapping(value="/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value="/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @GetMapping(value="/test")
+    public String test() {
+        return "test";
+    }
+
+    @PostMapping(value="/newuser")
+    public String addNewUser( Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "profile";
+    }
+}
---
+++ src/main/java/com/aficionado/sevice/UserService.java
@@ -0,0 +1,45 @@
+package com.aficionado.sevice;
+
+import com.aficionado.models.User;
+import com.aficionado.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserService {
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findByUserId(Long Id) {
+        return userRepository.findUserById(Id);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        String loggedInUserName = SecurityContextHolder.
+                getContext().getAuthentication().getName();
+
+        return findByUserName(loggedInUserName);
+    }
+
+
+}
