236c4965aa087f47fbc25f2207b745d7e9368c27
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-07 19:01:02 -0500

    routes to all pages complete

--- src/main/java/com/aficionado/site/controllers/MainController.java
+++ src/main/java/com/aficionado/site/controllers/MainController.java
@@ -11,4 +11,28 @@ public class MainController {
     public String index() {
         return "index";
     }
+
+    @GetMapping(value="/about")
+    public String about() { return "about"; }
+
+    @GetMapping(value="/login")
+    public String login() { return "login"; }
+
+    @GetMapping(value="/contact")
+    public String contact() { return "contact"; }
+
+    @GetMapping(value="/profile")
+    public String profile() { return "profile"; }
+
+    @GetMapping(value="/poduct")
+    public String product() { return "product"; }
+
+    @GetMapping(value="/signup")
+    public String signup() { return "signup"; }
+
+    @GetMapping(value="/confirm")
+    public String confirm() { return "confirm"; }
+
+    @GetMapping(value="/cart")
+    public String cart() { return "cart"; }
 }
