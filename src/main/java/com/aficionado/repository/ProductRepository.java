31ea52b7a5bf5fe90614004fc8790b2748aa23fe
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-09 09:51:16 -0500

    Product model and repository added

--- src/main/java/com/aficionado/repository/ProductRepository.java
+++ src/main/java/com/aficionado/repository/ProductRepository.java
@@ -1,12 +1,14 @@
-//package com.aficionado.repository;
-//
-//import java.util.List;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//
-//@Repository
-//public interface ProductRepository extends CrudRepository <Product, Long> {
-//
-//}
+package com.aficionado.repository;
+
+import java.util.List;
+
+import com.aficionado.models.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ProductRepository extends CrudRepository <Product, Long> {
+
+}
