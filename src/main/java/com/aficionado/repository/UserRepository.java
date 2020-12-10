bc2b5166be75c44e368ae5faed9a362727033046
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-10 14:40:14 -0500

    userservice complete

--- src/main/java/com/aficionado/repository/UserRepository.java
+++ src/main/java/com/aficionado/repository/UserRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.repository.CrudRepository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
-
 }
--- src/main/java/com/aficionado/sevice/UserService.java
+++ src/main/java/com/aficionado/sevice/UserService.java
@@ -1,45 +1,59 @@
 package com.aficionado.sevice;
 
+import com.aficionado.models.Product;
+import com.aficionado.models.Role;
 import com.aficionado.models.User;
+import com.aficionado.repository.RoleRepository;
 import com.aficionado.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
 
 public class UserService {
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
                        BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public User findByUserName(String userName) {
-        return userRepository.findByUsername(userName);
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 
-//    public User findByUserId(Long Id) {
-//        return userRepository.findUserById(Id);
-//    }
-
-    public void save(User user) {
+    public void saveExisting(User user) {
         userRepository.save(user);
     }
 
     public User saveNewUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         return userRepository.save(user);
     }
 
     public User getLoggedInUser() {
         String loggedInUserName = SecurityContextHolder.
                 getContext().getAuthentication().getName();
-
-        return findByUserName(loggedInUserName);
+        return findByUsername(loggedInUserName);
     }
 
+    public void updateCart(Map<Product, Integer> cart) {
+        User user = getLoggedInUser();
+        System.out.println(cart);
+        user.setCart(cart);
+        saveExisting(user);
+    }
 
 }
