2c7fc63b1cee2f145d975d25850f8f902d35819b
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-10 14:57:59 -0500

    auth controller complete, not tested

--- src/main/java/com/aficionado/controllers/AuthController.java
+++ src/main/java/com/aficionado/controllers/AuthController.java
@@ -2,16 +2,50 @@ package com.aficionado.controllers;
 
 import com.aficionado.models.User;
 import com.aficionado.repository.UserRepository;
+import com.aficionado.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 
 @Controller
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "login")
+    public String login() {
+        System.out.print("User logged in.");
+        return "login";
+    }
+
+    @GetMapping(value = "/signup")
+    public String registration(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "profile";
+    }
+
+    @PostMapping(value = "/signup")
+    public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+        User userExists = userService.findByUsername(user.getUsername());
+        if (userExists != null) {
+            bindingResult.rejectValue("username", "error.user", "Username is already taken");
+        }
+
+        if (!bindingResult.hasErrors()) {
+            userService.saveNewUser(user);
+            model.addAttribute("success", "Sign up successful");
+            model.addAttribute("user", new User());
+        }
+        return "login";
+    }
 
 }
