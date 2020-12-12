d1054062b591bf36245113e700bbb9426ea0625d
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-11 19:47:06 -0500

    merge to john-stripe

--- src/main/java/com/aficionado/controllers/AuthController.java
+++ src/main/java/com/aficionado/controllers/AuthController.java
@@ -9,24 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
- import com.aficionado.models.User;
- import com.aficionado.repository.UserRepository;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.ModelAttribute;
- import org.springframework.web.bind.annotation.PostMapping;
-
-
 import com.aficionado.models.User;
+
 import com.aficionado.sevice.UserService;
 
 
  @Controller
  public class AuthController {
 
-    @Autowired
+     @Autowired
     private UserService userService;
 
     @GetMapping(value = "/login")
