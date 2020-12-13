fd4261176d05c29c1815fe49982a3be8b4cde577
Aaron Toliver <flux.charlotte@gmail.com>
2020-12-13 13:48:30 -0500

    sigh

--- src/main/java/com/aficionado/controllers/MainController.java
+++ src/main/java/com/aficionado/controllers/MainController.java
@@ -1,20 +1,33 @@
 package com.aficionado.controllers;
 
 
+import com.aficionado.models.Product;
 import com.aficionado.models.User;
 import com.aficionado.repository.UserRepository;
+import com.aficionado.sevice.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping(value="/")
     public String index() {
         return "index";
+    }
+
+    @ModelAttribute("products")
+    public List<Product> products() {
+        return productService.findAll();
     }
 
     @GetMapping(value="/about")
